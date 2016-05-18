c12cc048228976a3065aaa44c586caa330fe184e
mfasman95 <matthew.f4sm4n@gmail.com>
2016-05-18 14:49:02 -0400

    Display data for creators when clicked

--- js/expandResult.js
+++ js/expandResult.js
@@ -16,9 +16,13 @@ function expandResult(e){
         case "comic":
             break;
         case "creators":
-            html += "<h1>" + imgClicked.dataset.fullname + "</h1>";
+            html += "<h1>" + imgClicked.dataset.fullName + "</h1>";
             html += "<img src='" + imgClicked.src + "'/>";
-            html += "<p>" + MarvelReturnedEntries[imgClicked.dataset.fullname].description + "</p>";
+            html += "<p>" + imgClicked.dataset.fullName + " has worked on:\n" +
+                MarvelReturnedEntries[imgClicked.dataset.fullName].comics.available + " Comics,\n" +
+                MarvelReturnedEntries[imgClicked.dataset.fullName].series.available + " Series,\n" +
+                MarvelReturnedEntries[imgClicked.dataset.fullName].stories.available + " Stories,\n" +
+                MarvelReturnedEntries[imgClicked.dataset.fullName].events.available + " Events</p>";
             break;
         case "events":
             html += "<h1>" + imgClicked.dataset.title + "</h1>";
