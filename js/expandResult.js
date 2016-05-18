d1084d9a724fbadb653630bea4b75ba9586f9e7a
astrodude80 <arl8315@rit.edu>
2016-05-18 14:47:44 -0400

    Change dataset attributes when images are loaded to the page. Add comic case for expand result

--- js/expandResult.js
+++ js/expandResult.js
@@ -14,6 +14,9 @@ function expandResult(e){
             html += "<p>" + MarvelReturnedEntries[imgClicked.dataset.name].description + "</p>";
             break;
         case "comic":
+            html += "<h1>" + imgClicked.dataset.title + "</h1>";
+            html += "<img src='" + imgClicked.src + "'/>";
+            html += "<p>" + MarvelReturnedEntries[imgClicked.dataset.title].description + "</p>";
             break;
         case "creators":
             html += "<h1>" + imgClicked.dataset.fullname + "</h1>";
